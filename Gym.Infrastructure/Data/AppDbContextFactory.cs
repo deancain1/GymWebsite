3dc660a5db8f1dd4ce4dd71974238d08624c2478
Dean Cain A. Ronquillo <caindeanw@gmail.com>
2025-11-07 23:36:02 +0800

    Add files via upload

---
+++ Gym.Infrastructure/Data/AppDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gym.Infrastructure.Data
+{
+    public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+
+            optionsBuilder.UseSqlServer(
+               "Server=localhost;Database=;Trusted_Connection=True;TrustServerCertificate=True;");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
