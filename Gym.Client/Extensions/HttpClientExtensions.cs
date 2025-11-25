b20eae43b7a2ee513f22d980000b48a325380139
caind <caindeanw@gmail.com>
2025-11-25 14:00:19 +0800

    Clean Program.cs

---
+++ Gym.Client/Extensions/HttpClientExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Gym.Client.Extensions
+{
+    public static class HttpClientExtensions
+    {
+        public static IServiceCollection AddMainApiClient(this IServiceCollection services, IConfiguration config)
+        {
+            var baseUrl = config["ApiSettings:BaseUrl"]
+                ?? throw new Exception("ApiSettings:BaseUrl is missing");
+
+            services.AddHttpClient("MainApi", client =>
+            {
+                client.BaseAddress = new Uri(baseUrl);
+            });
+
+           
+            services.AddScoped(sp =>
+                sp.GetRequiredService<IHttpClientFactory>().CreateClient("MainApi"));
+
+            return services;
+        }
+    }
+}
---
+++ Gym.Client/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Gym.Client.Interfaces;
+using Gym.Client.Security;
+using Gym.Client.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace Gym.Client.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddClientServices(this IServiceCollection services)
+        {
+          
+            services.AddScoped<CustomAuthStateProvider>();
+            services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
+
+         
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IMembershipService, MembershipService>();
+            services.AddScoped<IAttendanceService, AttendanceService>();
+
+            return services;
+        }
+    }
+}
